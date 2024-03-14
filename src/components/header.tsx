a9fb22470f3bff35410b0a4dff5f795fa9a9ac0d
Agata Vitoria <agatavitoriadelima@gmail.com>
2024-03-13 22:23:46 -0300

    feat: api integration created

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,20 +1,18 @@
 interface HeaderProps {
-	back?: boolean;
+  back?: boolean;
 }
 
 export function Header({ back }: HeaderProps) {
-	console.log(back);
-
-	return (
-		<header className="w-full p-6 flex flex-row justify-between items-center">
-			<h1 className="text-2xl text-sky-950 font-bold">Gift's List</h1>
-			{back && (
-				<button type="button">
-					<span className="font-bold text-base text-sky-950">
-						voltar para home &gt;
-					</span>
-				</button>
-			)}
-		</header>
-	);
+  return (
+    <header className="w-full p-6 flex flex-row justify-between items-center">
+      <h1 className="text-2xl text-sky-950 font-bold">Gift's List</h1>
+      {back && (
+        <button type="button">
+          <span className="font-bold text-base text-sky-950">
+            voltar para home &gt;
+          </span>
+        </button>
+      )}
+    </header>
+  );
 }
